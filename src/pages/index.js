b47f547c0a17e9567a6f2ed600f8b158eca4d388
Faith Lee <faithghlee@gmail.com>
2020-08-06 15:27:27 -0400

    add css bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import MinimalPortfolio from "@openarchitex/gatsby-theme-minimal-portfolio/src/templates/minimal-portfolio";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Home() {
   return <MinimalPortfolio />
 }
