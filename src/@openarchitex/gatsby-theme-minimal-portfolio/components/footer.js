c66cb651c4bb5d30fe57f3224a6b73e28a8fd819
Faith Lee <faithghlee@gmail.com>
2020-08-06 19:53:40 -0400

    Add Socialmedia Icons to Footer (#2)
    feat: add social media icons to footer section
    
    This adds LinkedIn and Facebook social media icons to the footer of the webpage.

---
+++ src/@openarchitex/gatsby-theme-minimal-portfolio/components/footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import ListGroup from "react-bootstrap/ListGroup";
+
+const Footer = () => (
+    <footer>
+        <div className="container-fluid">
+            <div className="mt-3 text-center">
+                <div className="row">
+                    <div className="mx-auto">
+                        <ListGroup horizontal>
+                            <ListGroup.Item className="border-0"><a aria-label="facebook" style={{color: "inherit"}} href="https://www.facebook.com/OpenArchitex-588167811850853/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2x"/></a></ListGroup.Item>
+                            <ListGroup.Item className="border-0"><a aria-label="linkedin" style={{color: "inherit"}} href="https://www.linkedin.com/company/openarchitex/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a></ListGroup.Item>
+                        </ListGroup>
+                    </div>
+                </div>
+                <p>
+                    <small>
+                        Powered by <a href="https://github.com/OpenArchitex" target="_blank" rel="noopener noreferrer">OpenArchitex</a>
+                        &copy;{new Date().getFullYear()}. Code licensed under an {` `}
+                        <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer">MIT License</a>.
+                    </small>
+                </p>
+            </div>
+        </div>
+    </footer>
+)
+
+export default Footer
