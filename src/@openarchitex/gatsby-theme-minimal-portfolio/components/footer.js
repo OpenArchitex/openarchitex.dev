d5fb14850a9b3d870935e116492080f51011a220
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-09-27 21:32:42 -0700

    feat: add GitHub footer icon
    This adds GitHub link to footer.js

--- src/@openarchitex/gatsby-theme-minimal-portfolio/components/footer.js
+++ src/@openarchitex/gatsby-theme-minimal-portfolio/components/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import ListGroup from "react-bootstrap/ListGroup";
 
 const Footer = () => (
@@ -12,6 +12,7 @@ const Footer = () => (
                         <ListGroup horizontal>
                             <ListGroup.Item className="border-0"><a aria-label="facebook" style={{color: "inherit"}} href="https://www.facebook.com/OpenArchitex-588167811850853/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2x"/></a></ListGroup.Item>
                             <ListGroup.Item className="border-0"><a aria-label="linkedin" style={{color: "inherit"}} href="https://www.linkedin.com/company/openarchitex/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a></ListGroup.Item>
+                            <ListGroup.Item className="border-0"><a aria-label="github" style={{color: "inherit"}} href="https://github.com/OpenArchitex" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="2x"/></a></ListGroup.Item>
                         </ListGroup>
                     </div>
                 </div>
